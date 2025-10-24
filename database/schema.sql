332a7eb1d8325ec5a732cdfef3a97b292c42a175
ChamasiDayarathne <chamasi2003@egmail.com>
2025-10-24 23:11:47 +0530

    Added database schema

--- database/schema.sql
+++ database/schema.sql
@@ -28,3 +28,35 @@ insert into Hotels (HotelName, Address, Contactno, Package01, Price01, Package02
     FOREIGN KEY (BookingID) REFERENCES Booking(BookingID),
     FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
 );
+create table Salons(
+    SalonNo int auto_increment primary key,
+SalonName varchar(100) not null,
+Address varchar(100),
+ContactNo char(20),
+PackageName1 varchar(100),
+    Price1 decimal(10,2),
+    PackageName2 varchar(100),
+Price2 decimal(10,2));
+   
+    insert into Salons(SalonName,Address,ContactNo,PackageName1,Price1,PackageName2,Price2)Values
+    ('Glamour Touch Salon','Nugegoda','077-4598721','Bridal Makeup',25000,'Bridal + Retouch',35000),
+    ('Royal Beauty Lounge','Maharagama','070-2298723','Full Package',40000,'Premium Package',55000),
+    ('Radiance Bridal Studio','Kirulapana','078-1198744','Makeup Only',20000,'Makeup + Hair Styling',30000),
+    ('Lotus Bridal Spa','Homagama','075-5598712','Pre-Wedding Package',15000,'Full Bridal Glow Package',25000);
+   
+    create table Cakes(
+    CShopID int auto_increment primary key,
+CShopName varchar(100) not null,
+Address varchar(100),
+ContactNo char(20),
+PackageName1 varchar(100),
+    Price1 decimal(10,2),
+    PackageName2 varchar(100),
+Price2 decimal(10,2));
+   
+    insert into Cakes(CShopName,Address,ContactNo,PackageName1,Price1,PackageName2,Price2)Values
+    ('Classic White Elegance','Nugegoda','077-4598721','3-Tier',30000,'4-Tier',40000),
+    ('Chocolate Romance','Homagama','071-8598726','2-Tier',25000,'3-Tier',35000),
+    ('Floral Garden Bliss','Kottawa','070-3398711','3-Tier',28000,'4-Tier',38000),
+    ('Custom Luxury Cake','Kirulapana','078-5998736','3-Tier',35000,'Customized',50000);
+   
